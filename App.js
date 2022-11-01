44a6d45b918dab21cd54f7c2a236126be2b1b778
Harrison South <hsouth95@hotmail.co.uk>
2022-11-01 13:19:22 +0000

    Create a very basic view of the app

--- App.js
+++ App.js
@@ -1,21 +1,62 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image, SafeAreaView } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up this code to start working on your app!</Text>
-      <Text>Apple bottoms jeans</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>Stock Swiper</Text>
+      <Image
+        style={styles.image}
+        source={require("./assets/cisco.jpg")}
+      />
+      <View style={styles.buttonContainer}>
+        <View style={styles.downButton}>
+          <Text style={styles.buttonText}>Down</Text>
+        </View>
+        <View style={styles.upButton}>
+          <Text style={styles.buttonText}>Up</Text>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#000",
     alignItems: "center",
+    flexDirection: "column",
+  },
+  text: {
+    color: "#fff",
+    fontSize: 32,
+    fontWeight: "bold",
+  },
+  image: {
+    height: 400,
+    width: "100%",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    flex: 1,
+    height: "100%",
+    width: "100%",
+    padding: 0,
     justifyContent: "center",
+  },
+  upButton: {
+    backgroundColor: "green",
+    justifyContent: "center",
+    flex: 1,
+  },
+  downButton: {
+    backgroundColor: "red",
+    justifyContent: "center",
+    flex: 1,
+  },
+  buttonText: {
+    color: "#fff",
+    textAlign: "center",
   },
 });

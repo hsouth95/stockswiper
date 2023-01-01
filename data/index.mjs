992c8ec812a5f4b3795a150ee4652cb22b391a3c
Harrison South <hsouth95@hotmail.co.uk>
2023-01-01 17:33:46 +0000

    Added lambda function to repo

---
+++ data/index.mjs
@@ -0,0 +1,50 @@
+import yahooFinance from 'yahoo-finance2';
+
+const POPULAR_SYMBOLS = [
+  "AAPL",
+  "MSFT",
+  "GOOG",
+  "AMZN",
+  "TSLA",
+  "BRK-B",
+  "XOM",
+  "V",
+  "WMT",
+  "NVDA",
+  "MA",
+  //"005930.KS", // SAMSUNG
+  "PFE",
+  "KO",
+  "META",
+  "PEP",
+  "SHEL",
+  "MCD",
+  "DIS",
+  "CSCO",
+  "ACN",
+  "CRM",
+  "ADBE",
+  //"NIKE",
+];
+
+let financeInfo = {};
+
+
+export const handler =  async function(event, context) {
+	if (Object.keys(financeInfo).length >= 1) {
+	  return financeInfo;
+	}
+	let yesterday = new Date()
+	yesterday.setDate(yesterday.getDate() - 1);
+	for(const symbol of POPULAR_SYMBOLS){
+	  let data = {};
+	  try {
+		  data = await yahooFinance.historical(symbol, { period1: yesterday, interval: "1d", });
+		  financeInfo[symbol] = data;
+	  } catch(e) {
+	    console.log(e);
+	  }
+	}
+	
+	return financeInfo;
+}
